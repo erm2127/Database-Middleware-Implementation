ede908b424ce19da734af83fb454ec8853babe1c
Ermias Taye <111532338+erm2127@users.noreply.github.com>
2025-06-13 11:20:33 +0300

    Update aichatbot_gemini.py

--- apps/ai-chat-bot/aichatbot_gemini.py
+++ apps/ai-chat-bot/aichatbot_gemini.py
@@ -3,4 +3,15 @@ from google import genai
 
 API_KEY = "AIzaSyBaDmWWU2wmm2w1h_PNSWDfX5AaOwtAz2E"
 
-client
+client = genai.client(api_key=API_KEY)
+
+while True:
+    userInput = input("you: ")
+    if userInput.lower() == "exit":
+        print("Ai chatbot : Goodbye")
+        break
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=userInput
+    )
+    print(f"\nAI chatbot: {response.text}")
